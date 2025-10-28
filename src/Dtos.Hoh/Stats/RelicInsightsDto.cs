d9e45053e4e022bef21c216ac24ad279b5202441
Ingweland <ingweland@proton.me>
2025-10-28 10:44:38 +0100

    Make FromLevel property optional in RelicInsightsDto to fix deserialization of absent default value.

--- src/Dtos.Hoh/Stats/RelicInsightsDto.cs
+++ src/Dtos.Hoh/Stats/RelicInsightsDto.cs
@@ -2,7 +2,7 @@ namespace Ingweland.Fog.Dtos.Hoh.Stats;
 
 public class RelicInsightsDto
 {
-    public required int FromLevel { get; init; }
+    public int FromLevel { get; init; }
     public required IReadOnlyCollection<string> Relics { get; init; }
     public required int ToLevel { get; init; }
     public required string UnitId { get; init; }
