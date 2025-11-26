19a91e4827d8d13734cc926a8035c02a2360d84e
Ingweland <ingweland@proton.me>
2025-11-26 17:28:48 +0100

    Adjust TimerTrigger times for cleanup functions

--- src/Functions/Functions/DatabaseCleanupTrigger.cs
+++ src/Functions/Functions/DatabaseCleanupTrigger.cs
@@ -4,12 +4,10 @@ using Microsoft.Extensions.Logging;
 
 namespace Ingweland.Fog.Functions.Functions;
 
-public class DatabaseCleanupTrigger(
-    IDatabaseCleanupService databaseCleanupService,
-    ILogger<DatabaseCleanupTrigger> logger)
+public class DatabaseCleanupTrigger(IDatabaseCleanupService databaseCleanupService)
 {
     [Function("DatabaseCleanupTrigger")]
-    public async Task Run([TimerTrigger("0 0 8 */1 * *")] TimerInfo myTimer)
+    public async Task Run([TimerTrigger("0 0 8 * * *")] TimerInfo myTimer)
     {
         await databaseCleanupService.RunAsync();
     }
--- src/Functions/Functions/TableStorageCleanupFunction.cs
+++ src/Functions/Functions/TableStorageCleanupFunction.cs
@@ -4,12 +4,10 @@ using Microsoft.Extensions.Logging;
 
 namespace Ingweland.Fog.Functions.Functions;
 
-public class TableStorageCleanupFunction(
-    IInGameRawDataTableRepository inGameRawDataTableRepository,
-    ILogger<TableStorageCleanupFunction> logger)
+public class TableStorageCleanupFunction(IInGameRawDataTableRepository inGameRawDataTableRepository)
 {
     [Function("TableStorageCleanupFunction")]
-    public async Task Run([TimerTrigger("0 0 0 * * *")] TimerInfo myTimer)
+    public async Task Run([TimerTrigger("0 0 8 * * *")] TimerInfo myTimer)
     {
         var cutOffDate = DateTime.UtcNow.AddMonths(-1);
         await inGameRawDataTableRepository.DeleteAllAsync(cutOffDate);
