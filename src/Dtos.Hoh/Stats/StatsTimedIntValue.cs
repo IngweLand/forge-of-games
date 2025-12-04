04a55bca69a4ff0643f8bb9faa23a868f046dc5a
Ingweland <ingweland@proton.me>
2025-12-04 01:56:50 +0100

    Add JsonIgnore attribute to `StatsTimedIntValue.Value`

--- src/Dtos.Hoh/Stats/StatsTimedIntValue.cs
+++ src/Dtos.Hoh/Stats/StatsTimedIntValue.cs
@@ -1,7 +1,11 @@
+using System.Text.Json.Serialization;
+
 namespace Ingweland.Fog.Dtos.Hoh.Stats;
 
 public class StatsTimedIntValue
 {
     public required DateTime Date { get; init; }
+    
+    [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
     public required int Value { get; init; }
 }
