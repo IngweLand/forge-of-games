f0ab462d7b7bdc29b3303c00b05b1ba02833c9b5
Ingweland <ingweland@proton.me>
2025-12-06 19:12:31 +0100

    Change `ServerContributions` type from `decimal` to `float` in `AnnualBudget`.

---
+++ src/Infrastructure/Migrations/20251206181239_AnnualBudget2.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ingweland.Fog.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AnnualBudget2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "ServerContributions",
+                table: "annual_budgets",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ServerContributions",
+                table: "annual_budgets",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+    }
+}
--- src/Models.Fog/Entities/AnnualBudget.cs
+++ src/Models.Fog/Entities/AnnualBudget.cs
@@ -3,6 +3,6 @@ namespace Ingweland.Fog.Models.Fog.Entities;
 public class AnnualBudget
 {
     public int Year { get; set; }
-    public decimal ServerContributions { get; set; }
+    public float ServerContributions { get; set; }
     public int ServerGoal { get; set; }
 }
