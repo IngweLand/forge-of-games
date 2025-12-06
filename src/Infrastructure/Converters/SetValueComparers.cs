9e874e5c5140d906d9598f958c6cb79cf0a919fa
Ingweland <ingweland@proton.me>
2025-12-06 18:53:48 +0100

    Update `SetValueComparer` to use `SetEquals` for improved set comparison

--- src/Infrastructure/Converters/SetValueComparers.cs
+++ src/Infrastructure/Converters/SetValueComparers.cs
@@ -2,7 +2,7 @@ using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace Ingweland.Fog.Infrastructure.Converters;
 
-public class SetValueComparer<T>() : ValueComparer<ISet<T>>((c1, c2) => c1!.SequenceEqual(c2!),
+public class SetValueComparer<T>() : ValueComparer<ISet<T>>((c1, c2) => c1!.SetEquals(c2!),
     c => c.Aggregate(0, (a, v) => HashCode.Combine(a, v!.GetHashCode())),
     c => c.ToHashSet());
 
