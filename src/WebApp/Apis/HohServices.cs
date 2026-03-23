ac9477cd879c6e78d7a1fb7e91cc2c3287aae384
Ingweland <ingweland@proton.me>
2026-03-23 20:59:47 +0100

    Remove unused dependencies and services from `HohServices`.

--- src/WebApp/Apis/HohServices.cs
+++ src/WebApp/Apis/HohServices.cs
@@ -1,41 +1,20 @@
-using Ingweland.Fog.Application.Client.Web.Services.Abstractions;
 using Ingweland.Fog.Application.Core.Services;
-using Ingweland.Fog.Application.Core.Services.Hoh.Abstractions;
 using Ingweland.Fog.Application.Server.Interfaces.Hoh;
 using Ingweland.Fog.Application.Server.Services.Hoh.Abstractions;
-using Ingweland.Fog.Shared.Helpers.Interfaces;
 using MediatR;
 
 namespace Ingweland.Fog.WebApp.Apis;
 
 public class HohServices(
     ILogger<HohServices> logger,
-    IUnitService unitService,
-    ICityService cityService,
-    IResearchService researchService,
-    ICommandCenterService commandCenterService,
-    ICommonService commonService,
-    IProtobufSerializer protobufSerializer,
     ICommandCenterProfileRepository commandCenterProfileRepository,
     IInGameStartupDataProcessingService inGameStartupDataProcessingService,
     IInGameStartupDataRepository inGameStartupDataRepository,
     IHohCityRepository hohCityRepository,
-    ICampaignService campaignService,
-    ITreasureHuntService treasureHuntService,
     IWikipediaService wikipediaService,
-    IRelicCoreDataService relicCoreDataService,
-    ICommandCenterSharingService commandCenterSharingService,
-    IEquipmentInsightsService equipmentInsightsService,
-    IMediator mediator,
-    IProtobufResponseFactory protobufResponseFactory)
+    IMediator mediator)
 {
-    public ICampaignService CampaignService { get; } = campaignService;
-    public ICityService CityService { get; } = cityService;
     public ICommandCenterProfileRepository CommandCenterProfileRepository { get; } = commandCenterProfileRepository;
-    public ICommandCenterService CommandCenterService { get; } = commandCenterService;
-    public ICommandCenterSharingService CommandCenterSharingService { get; } = commandCenterSharingService;
-    public ICommonService CommonService { get; } = commonService;
-    public IEquipmentInsightsService EquipmentInsightsService { get; } = equipmentInsightsService;
 
     public IHohCityRepository HohCityRepository { get; } = hohCityRepository;
 
@@ -47,11 +26,5 @@ public class HohServices(
 
     public ILogger<HohServices> Logger { get; } = logger;
     public IMediator Mediator { get; } = mediator;
-    public IProtobufResponseFactory ProtobufResponseFactory { get; } = protobufResponseFactory;
-    public IProtobufSerializer ProtobufSerializer { get; } = protobufSerializer;
-    public IRelicCoreDataService RelicCoreDataService { get; } = relicCoreDataService;
-    public IResearchService ResearchService { get; } = researchService;
-    public ITreasureHuntService TreasureHuntService { get; } = treasureHuntService;
-    public IUnitService UnitService { get; } = unitService;
     public IWikipediaService WikipediaService { get; } = wikipediaService;
 }
