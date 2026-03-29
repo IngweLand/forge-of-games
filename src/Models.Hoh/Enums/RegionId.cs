542cf8f4c9455aa8ebada82b5aa2c22f83384d26
Ingweland <ingweland@proton.me>
2026-03-29 13:45:44 +0200

    Add `CampaignRegionGroup` enum and associated region groupings in `CampaignRegionGroups` static class

--- src/Models.Hoh/Enums/RegionId.cs
+++ src/Models.Hoh/Enums/RegionId.cs
@@ -66,3 +66,53 @@ public enum RegionId
     FallOfTroy,
     AncientEgyptDungeon,
 }
+
+public enum CampaignRegionGroup
+{
+    Panganea,
+    DesertDelta,
+    EasternValley,
+    VolcanicJungle,
+    FrozenFjord,
+    EmeraldHighlands,
+}
+
+public static class CampaignRegionGroups
+{
+    public static IReadOnlyDictionary<CampaignRegionGroup, IReadOnlyCollection<RegionId>> All { get; } =
+        new Dictionary<CampaignRegionGroup, IReadOnlyCollection<RegionId>>
+        {
+            [CampaignRegionGroup.DesertDelta] = new List<RegionId>
+            {
+                RegionId.DesertDelta_1, RegionId.DesertDelta_2, RegionId.DesertDelta_3, RegionId.DesertDelta_4,
+                RegionId.DesertDelta_5, RegionId.DesertDelta_6, RegionId.DesertDelta_7, RegionId.DesertDelta_8,
+            },
+            [CampaignRegionGroup.EasternValley] = new List<RegionId>
+            {
+                RegionId.EasternValley_1, RegionId.EasternValley_2, RegionId.EasternValley_3, RegionId.EasternValley_4,
+                RegionId.EasternValley_5, RegionId.EasternValley_6, RegionId.EasternValley_7, RegionId.EasternValley_8,
+            },
+            [CampaignRegionGroup.EmeraldHighlands] = new List<RegionId>
+            {
+                RegionId.EmeraldHighlands_1, RegionId.EmeraldHighlands_2, RegionId.EmeraldHighlands_3,
+                RegionId.EmeraldHighlands_4, RegionId.EmeraldHighlands_5, RegionId.EmeraldHighlands_6,
+                RegionId.EmeraldHighlands_7, RegionId.EmeraldHighlands_8,
+            },
+            [CampaignRegionGroup.FrozenFjord] = new List<RegionId>
+            {
+                RegionId.FrozenFjord_1, RegionId.FrozenFjord_2, RegionId.FrozenFjord_3, RegionId.FrozenFjord_4,
+                RegionId.FrozenFjord_5, RegionId.FrozenFjord_6, RegionId.FrozenFjord_7, RegionId.FrozenFjord_8,
+            },
+            [CampaignRegionGroup.Panganea] = new List<RegionId>
+            {
+                RegionId.Panganea_1, RegionId.Panganea_2, RegionId.Panganea_3, RegionId.Panganea_4,
+                RegionId.Panganea_5, RegionId.Panganea_6, RegionId.Panganea_7, RegionId.Panganea_8,
+            },
+            [CampaignRegionGroup.VolcanicJungle] = new List<RegionId>
+            {
+                RegionId.VolcanicJungle_1, RegionId.VolcanicJungle_2, RegionId.VolcanicJungle_3,
+                RegionId.VolcanicJungle_4, RegionId.VolcanicJungle_5, RegionId.VolcanicJungle_6,
+                RegionId.VolcanicJungle_7, RegionId.VolcanicJungle_8,
+            },
+        };
+}
